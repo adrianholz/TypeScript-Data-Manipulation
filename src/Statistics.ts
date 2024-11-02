b0f0e71adb70836e3989527aecd388d15b98b19b
Adrian Holzschuh <adrian.holzschuh@gmail.com>
2024-11-02 15:24:37 -0700

    Count By (Status and Payment type)

--- src/Statistics.ts
+++ src/Statistics.ts
@@ -1,3 +1,5 @@
+import countBy from "./countBy.js";
+
 type TransactionValue = Transaction & { value: number };
 
 function filterValue(
@@ -9,15 +11,27 @@ function filterValue(
 export default class Statistics {
   private transactions;
   public total;
+  public payment;
+  public status;
 
   constructor(transactions: Transaction[]) {
     this.transactions = transactions;
     this.total = this.setTotal();
+    this.payment = this.setPayment();
+    this.status = this.setStatus();
   }
 
   private setTotal() {
     return this.transactions.filter(filterValue).reduce((acc, item) => {
       return acc + item.value;
     }, 0);
+  }
+
+  private setPayment() {
+    return countBy(this.transactions.map(({ payment }) => payment));
+  }
+
+  private setStatus() {
+    return countBy(this.transactions.map(({ status }) => status));
   }
 }
